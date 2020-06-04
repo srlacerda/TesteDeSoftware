9f31725f49233972f5b785757c455a3dc3bb5b04
srlacerda <diego.lacerda.alves@gmail.com>
2020-06-04 15:05:44 -0300

    Skip, Mensagens de Saída e Playlists

---
+++ 01 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class TesteNaoPassandoMotivoEspecifico
+    {
+
+        //[Fact(DisplayName ="Novo Cliente 2.0",Skip ="Nova versão 2.0 quebrando")]
+        [Fact(DisplayName = "Novo Cliente 2.0")]
+        [Trait("Categoria","Escapando de Testes")]
+        public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+        {
+            //Assert.True(false);
+            Assert.True(true);
+        }
+    }
+}
