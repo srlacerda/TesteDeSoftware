5e5f42ae2dbe393988b6747373225baecbb164fe
srlacerda <diego.lacerda.alves@gmail.com>
2020-06-05 12:55:45 -0300

    xunit.runner.console

--- 01 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
+++ 01 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -8,13 +8,13 @@ namespace Features.Tests
     public class TesteNaoPassandoMotivoEspecifico
     {
 
-        //[Fact(DisplayName ="Novo Cliente 2.0",Skip ="Nova versão 2.0 quebrando")]
-        [Fact(DisplayName = "Novo Cliente 2.0")]
+        [Fact(DisplayName ="Novo Cliente 2.0",Skip ="Nova versão 2.0 quebrando")]
+        //[Fact(DisplayName = "Novo Cliente 2.0")]
         [Trait("Categoria","Escapando de Testes")]
         public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
         {
-            //Assert.True(false);
-            Assert.True(true);
+            Assert.True(false);
+            //Assert.True(true);
         }
     }
 }
