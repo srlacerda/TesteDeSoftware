94f6d99117b1cb4f0de268fdfb7205415849cd28
srlacerda <diego.lacerda.alves@gmail.com>
2020-06-11 14:41:43 -0300

    teste

--- 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoItemTests.cs
@@ -12,6 +12,7 @@ namespace NerdStore.Vendas.Domain.Tests
         [Trait("Categoria", "Vendas - Pedido Item")] 
         public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
         {
+            
             // Arrange & Act & Assert
             Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Produto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
         }
