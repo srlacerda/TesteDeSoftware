71c53a737a520c0db2c65a1b9142275de305418b
srlacerda <diego.lacerda.alves@gmail.com>
2020-09-04 21:29:19 -0300

    Teste de Integração - Registro de Usuário - part01

--- 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -1,11 +1,13 @@
 ﻿using NerdStore.WebApp.MVC;
 using NerdStore.WebApp.Tests.Config;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UsuarioTests
     {
         private readonly IntegrationTestsFixture<StartupWebTests> _testFixture;
@@ -13,6 +15,38 @@ namespace NerdStore.WebApp.Tests
         public UsuarioTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
         {
             _testFixture = testsFixture;
+        }
+        
+        [Fact(DisplayName = "Realizar cadastro com sucesso")]
+        [Trait("Categoria","Integração Web Usuário")]
+        public async Task Usuario_RealziarCadastro_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var initialResponse = await _testFixture.Client.GetAsync(requestUri: "Identity/Account/Register");
+            initialResponse.EnsureSuccessStatusCode();
+
+            var email = "teste2@teste.com";
+            var formData = new Dictionary<string, string>
+            {
+                {"Input.Email", email},
+                {"Input.Passord","Teste@123"},
+                {"Input.ConfirmPassord","Teste@123"},
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+            };
+
+            // Act
+            var postResponse = await _testFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains(expectedSubstring: $"Hello {email}", responseString);
+
         }
     }
 }
